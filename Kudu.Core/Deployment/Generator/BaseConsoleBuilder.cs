40bb9892979657cb9a714ba6ed70fd3b32b900a9
Amit Apple <amitap@microsoft.com>
2013-10-04 17:22:57 -0700

    Added ability to deploy console apps using kudu as a simple worker website.

---
+++ Kudu.Core/Deployment/Generator/BaseConsoleBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Kudu.Contracts.Settings;
+using Kudu.Core.Infrastructure;
+
+namespace Kudu.Core.Deployment.Generator
+{
+    public abstract class BaseConsoleBuilder : GeneratorSiteBuilder
+    {
+        protected BaseConsoleBuilder(IEnvironment environment, IDeploymentSettingsManager settings, IBuildPropertyProvider propertyProvider, string sourcePath, string projectPath)
+            : base(environment, settings, propertyProvider, sourcePath)
+        {
+            ProjectPath = projectPath;
+        }
+
+        protected virtual string Command
+        {
+            get { return DeploymentSettings.GetValue(SettingsKeys.WorkerCommand); }
+        }
+
+        protected string ProjectPath { get; private set; }
+
+        public override async Task Build(DeploymentContext context)
+        {
+            string destinationPath = context.BuildTempPath;
+            string consoleWorkerPath = Path.Combine(Environment.ScriptPath, "ConsoleWorker");
+
+            CopyFile(consoleWorkerPath, destinationPath, "global.asax");
+            CopyFile(consoleWorkerPath, destinationPath, "web.config");
+
+            string scriptDirectoryPath = Path.Combine(destinationPath, "bin");
+            string scriptFilePath = Path.Combine(scriptDirectoryPath, "run_worker.cmd");
+            string scriptContent = "@echo off\n{0}\n".FormatInvariant(Command);
+
+            OperationManager.Attempt(() =>
+            {
+                FileSystemHelpers.EnsureDirectory(scriptDirectoryPath);
+                File.WriteAllText(scriptFilePath, scriptContent);
+            });
+
+            await base.Build(context);
+        }
+
+        private static void CopyFile(string sourcePath, string destinationPath, string fileName)
+        {
+            sourcePath = Path.Combine(sourcePath, fileName + ".template");
+            destinationPath = Path.Combine(destinationPath, fileName);
+            OperationManager.Attempt(() => File.Copy(sourcePath, destinationPath, overwrite: true));
+        }
+    }
+}
---
+++ Kudu.Core/Deployment/Generator/DotNetConsoleBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using Kudu.Contracts.Settings;
+using Kudu.Core.Infrastructure;
+
+namespace Kudu.Core.Deployment.Generator
+{
+    public class DotNetConsoleBuilder : BaseConsoleBuilder
+    {
+        private readonly string _solutionPath;
+
+        public DotNetConsoleBuilder(IEnvironment environment, IDeploymentSettingsManager settings, IBuildPropertyProvider propertyProvider, string sourcePath, string projectPath, string solutionPath)
+            : base(environment, settings, propertyProvider, sourcePath, projectPath)
+        {
+            _solutionPath = solutionPath;
+        }
+
+        protected override string Command
+        {
+            get { return base.Command ?? VsHelper.GetProjectExecutableName(ProjectPath); }
+        }
+
+        protected override string ScriptGeneratorCommandArguments
+        {
+            get
+            {
+                StringBuilder commandArguments = new StringBuilder();
+                commandArguments.AppendFormat("--dotNetConsole \"{0}\"", ProjectPath);
+
+                if (!String.IsNullOrEmpty(_solutionPath))
+                {
+                    commandArguments.AppendFormat(" --solutionFile \"{0}\"", _solutionPath);
+                }
+                else
+                {
+                    commandArguments.AppendFormat(" --no-solution", _solutionPath);
+                }
+
+                return commandArguments.ToString();
+            }
+        }
+
+        public override string ProjectType
+        {
+            get { return ".NET CONSOLE WORKER"; }
+        }
+    }
+}
